b16313239bbc16c2ddd035501316cb677fbc5d43
pathfinder <chm2920@gmail.com>
2013-03-02 11:34:11 +0800

    update

--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -4,43 +4,9 @@ class StartController < ApplicationController
   end
   
   def upload
-    require 'RMagick'
-    @upload = params[:imgfile]
-    @image = Image.new(@upload)
-    @image.save
-    
-    @img = Magick::Image.read(@image.path).first
-    
-    p = 40
-    q = 30
-    
-    w = @img.columns
-    h = @img.rows
-    
-    a = w / p
-    b = h / q
-    
-    # @test = @img.pixel_color(0, 0)
-#     
-    # 0.upto @img.columns do |x|
-      # i = (x / a).to_i
-      # 0.upto @img.rows do |y|
-        # @test = @img.pixel_color(x, y)
-        # 0.299*R + 0.587*G + 0.114*B
-        # j = (y / b).to_i
-        # if (i+j) % 2 == 0
-          # @img.pixel_color(x, y, "white")
-        # else
-          # @img.pixel_color(x, y, "black")
-        # end
-      # end
-    # end
-    
-    img_b = Magick::Image.new(w, h, Magick::HatchFill.new('white', 'black', a))
-    
-    Magick::Draw.new.circle(w/2, h/2, w/2 - 40, h/2).draw(img_b);
-    
-    img_b.write(@image.path_b)
+    @image = Image.new(params[:imgfile])
+    @img = @image.save
+    @test = @img.pixel_color(0, 0)
   end
   
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,7 @@
+require 'RMagick'
 class Image
   
-  attr :bits, :path, :path_b, :rel_path, :rel_path_b
+  attr :obj, :bits, :path, :path_b, :rel_path, :rel_path_b, :weights
   
   def initialize(data)
     @bits = data
@@ -19,9 +20,76 @@ class Image
     
     @rel_path = File.join(upload_path + 'a' + extName)
     @rel_path_b = File.join(upload_path + 'b' + extName)
+    
     File.open(@path, "wb") { |file|
       file.write(@bits.read)
     }
-  end  
+    
+    render_b
+    
+    @obj
+  end
+  
+  def render_b    
+    @obj = Magick::Image.read(@path).first
+    @obj.pixel_color(0, 0, "white")
+    
+    p = 160
+    q = 120
+    
+    w = @obj.columns
+    h = @obj.rows
+    
+    a = w / p
+    b = h / q
+    
+    @max = cal_max_lu
+    
+    img_b = Magick::Image.new(w, h, Magick::HatchFill.new('white', 'black', a))
+    
+    @weights = []
+    0.upto q-1 do |y|
+      @weights[y] = []
+      0.upto p-1 do |x|
+        sx = a * x
+        sy = b * y
+        ex = a * (x + 1)
+        ey = b * (y + 1)
+        weight = cal_weight(sx, sy, ex, ey)
+        @weights[y][x] = weight
+        
+        if weight != 0
+          midx = sx + a/2
+          midy = sy + b/2
+          rdx = sx + (a/2 * weight) / 10
+          
+          Magick::Draw.new.circle(midx, midy, rdx, midy).draw(img_b)
+        end
+      end
+    end
+    
+    img_b.write(@path_b)
+  end
+  
+  def cal_lu(p_color)
+    0.299 * p_color.red + 0.587 * p_color.green + 0.114 * p_color.blue
+  end
+  
+  def cal_weight(sx, sy, ex, ey)
+    sum = 0
+    sy.upto ey-1 do |y|
+      sx.upto ex-1 do |x|
+        p_color = @obj.pixel_color(x, y)
+        lu = cal_lu(p_color)
+        sum =+ (lu * 10 / @max).to_i
+      end
+    end
+    sum
+  end
+  
+  
+  def cal_max_lu
+    0.299 * 65535 + 0.587 * 65535 + 0.114 * 65535
+  end
   
 end
